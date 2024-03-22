fcdde92788c52d1e414af95ff6374a89ced7ff1a
SuperYuro <a.gpgtdmgp@gmail.com>
2024-03-22 16:55:16 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/kosenconf_qr_scanner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kosenconf_qr_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/superyuro/kosenconf_qr_scanner/kosenconf_qr_scanner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.superyuro.kosenconf_qr_scanner.kosenconf_qr_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
