c5f303af9061bd25aec17a68c13fea9e9dd4b6da
SuperYuro <a.gpgtdmgp@gmail.com>
2024-03-24 13:50:48 +0900

    feat: add QR Scanner screen

---
+++ lib/screens/qr_scanner.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class QRScanner extends StatefulWidget {
+  const QRScanner({super.key});
+
+  @override
+  State<QRScanner> createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("QRScanner"),
+      ),
+      body: const Text("QRScanner"),
+    );
+  }
+}
